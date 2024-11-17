3724367f1d92cd43c63c44c54b3b3d16f3f75e8e
JavierCedo <salcedojavier115@gmail.com>
2024-11-16 21:24:41 -0400

    Codigo 09 - Funcional(Ajustar detalles)

--- Codigo.py
+++ Codigo.py
@@ -24,7 +24,7 @@ far1 = {
     "Diazepam": ["Diazepam"],
     "Clonazepam": ["Clonazepam"],
     "Acetaminofen": ["Acetaminofen"],
-    "Amoxicilina con ácido clavulánico": ["Amoxicilina", "Ácido clavulánico", "Otro"],
+    "Amoxicilina con ácido clavulánico": ["Amoxicilina", "Ácido clavulánico", "Javi er"],
 } 
     
 far2 = {
@@ -55,7 +55,6 @@ far3 = {
     "Paracetamol": ["Paracetamol"],
     "Ibuprofeno": ["Ibuprofeno"],
     "Naproxeno sódico": ["Naproxeno"],
-    #"Amoxicilina con ácido clavulánico": ["Amoxicilina", "Ácido clavulánico"],
     "Diclofenaco potásico": ["Diclofenaco"],
     "Cetirizina": ["Cetirizina"],
     "Famotidina": ["Famotidina"],
@@ -67,46 +66,56 @@ far3 = {
     "Amlodipino besilato": ["Amlodipino"],
     "Hidroclorotiazida": ["Hidroclorotiazida"],
     "Levotiroxina sódica": ["Levotiroxina"],
-    "Sertralina": ["Sertralina"],
+    "Sertralina": ["Sertralina", "Otro"],
     "Venlafaxina": ["Venlafaxina"],
     "Olanzapina": ["Olanzapina"],
     "Carbamazepina": ["Ácido clavulánico"],
-    "Gabapentina": ["Gabapentina", "Otro"],
+    "Gabapentina": ["Gabapentina", "Javi er"],
     "Diazepam": ["Enalapril maleato"],
 }
 
+
 ## Nombres de las farmacias
-Nom_far = ["far1","far2","far3"]
+
+Nom_far = ["Farmacia1","Farmacia2","Farmacia3"]
+
 
 ## Convierte la base de tados de ejemplo en un archivo CSV
+
 farmacias = [far1, far2, far3]
 df0 = pd.DataFrame(farmacias, index= Nom_far)
 df0.to_csv('far_fic.csv', index=False)
 
+
 ## Leer el archivo CSV con la base de datos
+
 df = pd.read_csv('far_fic.csv')
 df.index = Nom_far
 
 
-## Entrada de datos
+## Entrada de datos             Me dicamentos que han dado problemas y con lo que se estudian diferentes casos
 
-
-#Med = "Alprazolam"
-Med = "Enalapril"
+Med = "Alprazolam"
+#Med = "Enalapril"
 #Med = "Amoxicilina con ácido clavulánico"
+#Med = "Paracetamol"
+
 print("Medicamento a buscar:", Med)
 
-# Almacenar datos de los resultado de busqueda
+
+## Almacenar datos de los resultado de busqueda
+
 Med_bus_1 = {}
 Com_bus_1 = {}
 Med_sim_bus_2 = {}
+similar_med = {}
 
-## Busqueda del medicamento en la farmacia
+
+## Busqueda del medicamento en la farmacia y sus componentes
 
 if Med in df:
     # Serie del medicamento "Med"
     df_Med = df[Med]
-
     # Indice y valor de la serie por farmacia
     for idx, val in df_Med.items():
         if pd.isnull(val):
@@ -116,39 +125,20 @@ if Med in df:
         else:
             # Si el medicamnto se encuestra
             Med_bus_1[idx] = "Esta"
-            val = val.replace("[","").replace("]","").replace("'"," ").split(', ')
+            val = val.replace("[","").replace("]","").replace("'","").split(', ')
             Com_bus_1[idx] = val
 else:
     # Si el medifamento no esta en ninguna farmacia
     print(f"El medicamento {Med} no está disponible en ninguna farmacia")
 
-
-## Resultado de la busqueda del medicamento y sus componentes
-
 ser_Med_bus_1 = pd.Series(Med_bus_1)
-print("-"*40)
-print("*En que farmacia esta el medicamento*")
-print(ser_Med_bus_1.to_string())
-print("-"*40)
-print("*Componentes del medicamento*")
 df_Com_bus_1 = pd.Series(Com_bus_1)
-print(df_Com_bus_1.to_string())
-print("-"*40)
-
-
-## Busqueda de los componentes del medicamento en otras farmacias
-
-# Serie que muestra en que farmacia no esta el medicamento
-print("*Farmacia donde no esta el medicamento*")
 ser_Med_sim_bus_2 = pd.Series(Med_sim_bus_2)
-print(ser_Med_sim_bus_2.to_string())
-print("-"*40)
+
+
+## Búsqueda de un medicamento similar en farmacias donde no está el original
 
 nan_indices= ser_Med_sim_bus_2[pd.isnull(ser_Med_sim_bus_2)].index 
-
-# Búsqueda de un medicamento similar en farmacias donde no está el original 
-similar_med = {}
-
 
 for idx_far in nan_indices: 
     for idx, val in df_Com_bus_1.items(): 
@@ -157,27 +147,41 @@ for idx_far in nan_indices:
             for sub_comp in comp:
                 valores_indice = df.loc[idx_far].to_list()
                 for i in valores_indice:
-                    i = str(i).replace("'","").replace("[","").replace("]","").split(",")
-                    sub_comp = sub_comp.replace("'", "")
-                    for sub_i in i:
-                        if sub_i == sub_comp:
-                            valor = str(i)
-                            indice = idx_far
-                            columna_encontrada = df.columns[(df.loc[indice] == valor)].tolist()
-                            similar_med[idx_far] = columna_encontrada
-                        #else:
-                        #    similar_med[idx_far] = "No existe similar"
+                    if isinstance(i, str):
+                        sub_comp = sub_comp.replace("'", "")
+                        i = eval(i)
+                        for sub_i in i:
+                            if sub_i == sub_comp:
+                                valor = str(i)
+                                indice = idx_far
+                                columna_encontrada = df.columns[(df.loc[indice] == valor)].tolist()
+                                similar_med[idx_far] = columna_encontrada
+                    else:
+                        pass
+
+ser_similar_med = pd.Series(similar_med)
+
+
+## Busqueda del medicamento en la farmacia y sus componentes
+
+print("-"*40)
+print("*En que farmacia esta el medicamento*")
+print(ser_Med_bus_1.to_string())
+print("-"*40)
+print("*Componentes del medicamento*")
+print(df_Com_bus_1.to_string())
+print("-"*40)
+
+## Busqueda de los componentes del medicamento en otras farmacias
+
+print("*Farmacia donde no esta el medicamento*")
+print(ser_Med_sim_bus_2.to_string())
+print("-"*40)
+
+## Búsqueda de un medicamento similar en farmacias donde no está el original
 
 print("*Farmacia donde existe uno similar*")
-ser_similar_med = pd.Series(similar_med)
 print (ser_similar_med.to_string())
-
-"""
-Falta corregir
-
-Cuando se tiene el medicamento y los componestes al buscar un medicamento similar no 
-busca y compara por cada componente. Lo hace por lista, no por elemento de la lista.
-"""
 
 
 """
