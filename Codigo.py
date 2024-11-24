3bbd644fdccf33dd9047183ace5cdc037c21414d
JavierCedo <salcedojavier115@gmail.com>
2024-11-23 23:38:48 -0400

    Limpieza de data - 3BDD

--- Codigo.py
+++ Codigo.py
@@ -11,7 +11,14 @@ os.environ['KAGGLE_USERNAME'] = 'javiercedo115'
 os.environ['KAGGLE_KEY'] = '70489924770b5b49f1db3a893bf8839a'
 dataset = 'singhnavjot2062001/11000-medicine-details'
 kaggle.api.dataset_download_files(dataset, path='.', unzip=True)
+dataset = 'ujjwalaggarwal402/medicine-dataset'
+kaggle.api.dataset_download_files(dataset, path='.', unzip=True)
 '''
+
+os.environ['KAGGLE_USERNAME'] = 'javiercedo115'
+os.environ['KAGGLE_KEY'] = '70489924770b5b49f1db3a893bf8839a'
+dataset = 'shudhanshusingh/az-medicine-dataset-of-india'
+kaggle.api.dataset_download_files(dataset, path='.', unzip=True)
 
 ''' ## Cosas a hacer
  Separar "Medicine Name" en tres columnas, Nombre, cantidad, presentacion, imagen del medicamento.
@@ -21,135 +28,57 @@ kaggle.api.dataset_download_files(dataset, path='.', unzip=True)
 '''
 
 
-###############         Base de datos          ################# Cargar los datos
+###############         Base de datos          #################
+
+
+
+
+
+
+
+
+''' #Componentes Medicine_Details.csv
 data = pd.read_csv("Medicine_Details.csv", index_col=False)
 
-# Limpiar espacios en blanco en los nombres de las columnas
-data.columns = data.columns.str.strip()
-
-# arreglar
-# Función para separar nombre, dosis y presentación
-
-def separar_nombre_dosis_presentacion(medicamento):
-    partes = medicamento.split()
-    if len(partes) < 2:
-        return pd.Series([medicamento, None, None])  # Devuelve el nombre y None para dosis y presentación
-    
-    # La última parte es la presentación, la penúltima es la dosis
-    presentacion = partes[-1]
-    dosis = partes[-2]
-    
-    # El resto es el nombre
-    nombre = ' '.join(partes[:-2])
-    
-    # Si la presentación tiene más de una palabra (ejemplo: "Nasal Spray")
-    if len(partes) > 3:
-        presentacion = ' '.join(partes[-2:])
-        dosis = partes[-3]
-    
-    return pd.Series([nombre, dosis, presentacion])
-# Aplicar la función de separación
-data[['Nombre', 'Dosis', 'Presentacion']] = data['Medicine Name'].apply(separar_nombre_dosis_presentacion)
-
-# Separar "Composition" en dos columnas: nombre_comp, cantidad_comp
-data[['nombre_comp', 'cantidad_comp']] = data['Composition'].str.split('+', n=1, expand=True)
-
-# Eliminar filas donde 'Nombre' esté vacío
-data = data[data['Nombre'].notnull() & (data['Nombre'] != '')]
-
-data = data.rename(columns={'Uses': 'Usos', 'Side_effects':'EfectosSecundarios'})
-
-df = data[['Nombre', 'Dosis', 'Presentacion', 'Composition', 'Usos', 'EfectosSecundarios']]
-
-
-prueba = data[['Nombre','Composition',]]
-
-prueba.apply(df.apply(lambda x: pd.Series([1, 2], index=['foo', 'bar']), axis=1))
-
-print(tabulate(prueba.head(20)))
-
-
-### Farmacias Ficticias
-
-df1 = df.sample(n=len(df)//3, replace=True)
-df2 = df.sample(n=len(df)//3, replace=True)
-df3 = df.sample(n=len(df)//3, replace=True)
-
-df1 = df1.drop_duplicates(subset=['Nombre', 'Dosis', 'Presentacion', 'Composition'])
-df2 = df2.drop_duplicates(subset=['Nombre', 'Dosis', 'Presentacion', 'Composition'])
-df3 = df3.drop_duplicates(subset=['Nombre', 'Dosis', 'Presentacion', 'Composition'])
-
-df1 = df1.set_index('Nombre')
-df2 = df2.set_index('Nombre')
-df3 = df3.set_index('Nombre')
-
-#print(tabulate(df1.info()))
-
-list_farms = {'Farmacia1':df1, 'Farmacia2':df2, 'Farmacia3':df3}
-
-###############         Codigo          ################
-
-'''
-# Entrada de datos (falta arreglar para la interfaz)
-
-       Med = input("Medicamento a buscar:").capitalize()
-'''
-
-##Me dicamentos que han dado problemas y con lo que se estudian diferentes casos
-
-med = "Azithral"
-
-## Almacenar datos de los resultado de busqueda
-
-farm_esta_med = {}
-farm_y_comp = {}
-farm_no_med = {}
-far_med_sml = {}
-
-## Donde esta el medicamento, sus componentes y donde no esta el medicamento
-
-for nom_farm, df_farm in list_farms.items():
-    if med in df_farm.index:
-        comp = df_farm.loc[med, "Composition"]
-        if type(comp) == pd.core.series.Series:
-            for idx, val in comp.items():
-                farm_y_comp[nom_farm] = val
-                farm_esta_med[nom_farm] = f"Esta {med}"
+# Función para separar los componentes y sub-separar en nombre y cantidad
+def separar_componentes(row):
+    componentes = row['Composition'].split(' + ')
+    for i, componente in enumerate(componentes):
+        if ' (' in componente and componente.endswith(')'):
+            nombre, cantidad = componente.rsplit(' (', 1)
+            cantidad = cantidad.rstrip(')')
         else:
-            farm_y_comp[nom_farm] = comp
-            farm_esta_med[nom_farm] = f"Esta {med}"
-            
-    else:
-        farm_no_med[nom_farm] = f"No esta {med}"
-        
-for nom_farm, df_farm in list_farms.items():
-    for farm, comp in farm_y_comp.items():
-        ###                         Hacer bucle para comp que va a ser una lista ya hacer lo mismo para hacer la comparacion y buscar en el df.
-        valor = comp
-        columna = 'Composition'
-        indice = df_farm.index[df_farm[columna] == valor]
-        indice = pd.Series(indice)
-        #print(comp, "a buecar en", nom_farm )
-        #print("lista similares", indice)
-        lista = []
-        for idx, val in indice.items():
-            if str(med) != str(val):
-                lista.append(val)
-                far_med_sml[nom_farm] = lista
+            nombre = componente
+            cantidad = ''
+        row[f'nombre_{i+1}'] = nombre
+        row[f'cantidad_{i+1}'] = cantidad
+    return row
 
-'''
-print("Medicamento a buscar:", med)
-print("En que farmacia esta")
-print(farm_esta_med)
-print("componestes por farmacia")
-print(farm_y_comp)
-print("En que farmacia no esta")
-print(farm_no_med)
-print("Similares")
-print(far_med_sml)
+df = data.apply(separar_componentes, axis=1)
+
+df = df.fillna('')
+
+columnas_intercaladas = []
+max_componentes = max(int(col.split('_')[1]) for col in df.columns if col.startswith('nombre_'))
+
+for i in range(1, max_componentes + 1):
+    columnas_intercaladas.append(f'nombre_{i}')
+    columnas_intercaladas.append(f'cantidad_{i}')
+
+componentes = df[columnas_intercaladas]
+
+from tabulate import tabulate
+
+# Mostrar los nombres de las columnas con tabulate
+print(tabulate(componentes.head(5)))
 '''
 
-#''' ## Graficas
+
+
+
+
+
+''' ## Graficas
 # Definición de la función para graficar gráficos circulares simplificados
 def plot_top_pie_chart(data, column, top_n=10):
     top_data = data[column].value_counts().nlargest(top_n)
@@ -208,4 +137,89 @@ if url_imagen:
     print(f"Abrir URL de la imagen para '{medicamento_principal}': {url_imagen}")
 else:
     print(f"No se encontró imagen para el medicamento '{medicamento_principal}'.")
-#'''
+'''
+
+
+
+
+
+
+'''
+
+### Farmacias Ficticias
+
+df1 = df.sample(n=len(df)//3, replace=True)
+df2 = df.sample(n=len(df)//3, replace=True)
+df3 = df.sample(n=len(df)//3, replace=True)
+
+df1 = df1.drop_duplicates(subset=['Nombre', 'Dosis', 'Presentacion', 'Composition'])
+df2 = df2.drop_duplicates(subset=['Nombre', 'Dosis', 'Presentacion', 'Composition'])
+df3 = df3.drop_duplicates(subset=['Nombre', 'Dosis', 'Presentacion', 'Composition'])
+
+df1 = df1.set_index('Nombre')
+df2 = df2.set_index('Nombre')
+df3 = df3.set_index('Nombre')
+
+#print(tabulate(df1.info()))
+
+list_farms = {'Farmacia1':df1, 'Farmacia2':df2, 'Farmacia3':df3}
+
+
+###############         Codigo          ################
+
+# Entrada de datos (falta arreglar para la interfaz)
+
+       Med = input("Medicamento a buscar:").capitalize()
+
+##Me dicamentos que han dado problemas y con lo que se estudian diferentes casos
+
+med = "Azithral"
+
+## Almacenar datos de los resultado de busqueda
+
+farm_esta_med = {}
+farm_y_comp = {}
+farm_no_med = {}
+far_med_sml = {}
+
+## Donde esta el medicamento, sus componentes y donde no esta el medicamento
+
+for nom_farm, df_farm in list_farms.items():
+    if med in df_farm.index:
+        comp = df_farm.loc[med, "Composition"]
+        if type(comp) == pd.core.series.Series:
+            for idx, val in comp.items():
+                farm_y_comp[nom_farm] = val
+                farm_esta_med[nom_farm] = f"Esta {med}"
+        else:
+            farm_y_comp[nom_farm] = comp
+            farm_esta_med[nom_farm] = f"Esta {med}"
+            
+    else:
+        farm_no_med[nom_farm] = f"No esta {med}"
+        
+for nom_farm, df_farm in list_farms.items():
+    for farm, comp in farm_y_comp.items():
+        ###                         Hacer bucle para comp que va a ser una lista ya hacer lo mismo para hacer la comparacion y buscar en el df.
+        valor = comp
+        columna = 'Composition'
+        indice = df_farm.index[df_farm[columna] == valor]
+        indice = pd.Series(indice)
+        #print(comp, "a buecar en", nom_farm )
+        #print("lista similares", indice)
+        lista = []
+        for idx, val in indice.items():
+            if str(med) != str(val):
+                lista.append(val)
+                far_med_sml[nom_farm] = lista
+
+print("Medicamento a buscar:", med)
+print("En que farmacia esta")
+print(farm_esta_med)
+print("componestes por farmacia")
+print(farm_y_comp)
+print("En que farmacia no esta")
+print(farm_no_med)
+print("Similares")
+print(far_med_sml)
+'''
